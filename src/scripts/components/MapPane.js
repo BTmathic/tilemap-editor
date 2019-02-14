dac7a81c972f6484bd006991a33c1f621fa2bfb9
Alexander Molnar <mathic@gmail.com>
2019-02-13 21:59:32 -0500

    Add mouse hover tracking on MapPane

--- src/scripts/components/MapPane.js
+++ src/scripts/components/MapPane.js
@@ -2,13 +2,70 @@ import React from 'react';
 import Tile from './Tile';
 
 const map = [
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
+  'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white',
   'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white'
 ];
 
-export default () => (
-  <div className='map-pane'>
-    {map.map((tile, index) => {
-      return <Tile colour={tile} key={tile + index} tileType={'map'} />
-    })}
-  </div>
-);
+export default class MapPane extends React.Component {
+  state = {
+    map,
+    mouseX: 0,
+    mouseY: 0,
+    tile: null
+  }
+
+  onMouseMove = (e) => {
+    e.persist();
+    const oldTile = this.state.tile;
+    const mouseX = e.clientX - this.state.offsetX;
+    const mouseY = e.clientY - this.state.offsetY;
+    const tile = Math.floor(mouseX/34) + 20*Math.floor(mouseY/34);
+    const map = this.state.map;
+    map[tile] = 'black';
+    if (tile !== oldTile) {
+      map[oldTile] = 'white';
+    }
+    this.setState(() => ({
+      map,
+      mouseX,
+      mouseY,
+      tile
+    }));
+  }
+
+  componentDidMount() {
+    this.setState(() => ({
+      offsetX: this.mapPane.getBoundingClientRect().x,
+      offsetY: Math.floor(this.mapPane.getBoundingClientRect().y)
+    }));
+  }
+
+  render() {
+    return (
+      <div className='map-pane'
+        onMouseMove={this.onMouseMove}
+        ref={(mapPane) => this.mapPane = mapPane}
+      >
+        {map.map((tile, index) => {
+          return <Tile colour={tile} key={tile + index} tileType={'map'} />
+        })}
+      </div>
+    );
+  }
+}
